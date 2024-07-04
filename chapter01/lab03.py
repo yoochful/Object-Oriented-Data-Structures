23da107bcbbf6979f015e075f92611a40acc960a
yoochful <bitoey2016@gmail.com>
2024-07-04 23:55:23 +0700

    update function

--- chapter01/lab03.py
+++ chapter01/lab03.py
@@ -14,3 +14,29 @@
 # Collection of distinct numbers:
 #  [[3, 2, 1, 1], [2, 3, 1, 1], [2, 1, 3, 1], [2, 1, 1, 3], [3, 1, 2, 1], [1, 3, 2, 1], [1, 2, 3, 1], [1, 2, 1, 3], [3, 1, 1, 2], [1, 3, 1, 2], [1, 1, 3, 2], [1, 1, 2, 3], [3, 2, 1, 1], [2, 3, 1, 1], [2, 1, 3, 1], [2, 1, 1, 3], [3, 1, 2, 1], [1, 3, 2, 1], [1, 2, 3, 1], [1, 2, 1, 3], [3, 1, 1, 2], [1, 3, 1, 2], [1, 1, 3, 2], [1, 1, 2, 3]]
 
+
+input = input("Input: ")
+input_list = list(map(int, input.split(',')))
+
+def permute(num):
+    result = []
+
+    if len(num) == 0:
+        return []
+    
+    if len(num) == 1:
+        return [num]
+    
+    for i in range(len(num)):
+        current_num = num[i]
+        remaining_num = num[:i] + num[i+1:]
+        for p in permute(remaining_num):
+            result.append([current_num] + p)
+    
+    return result
+
+permuted_list = permute(input_list)
+
+print("Original Collection: ",input_list)
+print("Collection of distinct numbers:")
+print(permuted_list)
--- chapter01/lab05.py
+++ chapter01/lab05.py
@@ -20,26 +20,26 @@
 # Enter All Bid : 5 10 20 20 10
 # error : have more than one highest bid
 
+bids_input = input("Enter All Bid: ").split()
+bids = [int(bid) for bid in bids_input]
 
-
-def vickrey_auction():
-    bids = input("Enter All Bid: ").split()
-    bids = list(map(int, bids))
+def vickrey_auction(bids):
     
-    if len(bids) < 2:
-        print("not enough bidder")
-        return
-
-    unique_bids = list(set(bids))
-    unique_bids.sort(reverse=True)
+    not_repeat_bids = list(set(bids))
     
-    if bids.count(unique_bids[0]) > 1:
-        print("error : have more than one highest bid")
-        return
+    if len(not_repeat_bids) < 2:
+        return "not enough bidder"
+    
+    highest_bid = max(not_repeat_bids)
+    not_repeat_bids.remove(highest_bid)
+    
+    if bids.count(highest_bid) > 1:
+        return "error : have more than one highest bid"
+    
+    second_highest_bid = max(not_repeat_bids)
+    return f"winner bid is {highest_bid} need to pay {second_highest_bid}"
 
-    highest_bid = unique_bids[0]
-    second_highest_bid = unique_bids[1]
-
-    print(f"winner bid is {highest_bid} need to pay {second_highest_bid}")
+result = vickrey_auction(bids)
+print(result)
 
 
