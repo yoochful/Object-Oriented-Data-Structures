43e848d73c27d99380ccc59cfd8569640f2b1a30
yoochful <bitoey2016@gmail.com>
2024-07-31 23:55:15 +0700

    review2

---
+++ review-BF-test/stack03.py
@@ -0,0 +1,130 @@
+# Chapter : 3 - item : 3 - Color_Crush
+
+# หลังจากกฤษฎาล้างจานเสร็จ ก็ได้มาเล่นเกมส์ที่กำลังเป็นที่นิยมทั่วโลกในตอนนี้   Microsoft Flight Simulator ?  Fall Guys ?  Valorant ?  "ผิดทั้งหมด!" กฤษฎาได้กล่าวไว้  เกมที่กำลังเป็นที่นิยมคือ Color Crush ต่างหาก   
+# โดยเกมนี้จะเป็นการนำสีมาเรียงต่อกัน โดยสีจะหายไปก็ต่อเมื่อมีการเรียงสีเหมือนกันครบ 3 อัน เช่น  A B B B A  -> A A เนื่องจาก B เรียงติดกัน 3 ตัวทำให้ระเบิดหายไปโดยที่สีจะมีทั้งหมด 26 สี และจะถูกแทนด้วย A - Z  โดยถ้าหากมีการระเบิดตั้งแต่ 2 ครั้งขึ้นไปจะแสดง Combo ขึ้นมา
+
+# โดยเมื่อการระเบิดสิ้นสุดลงให้แสดงจำนวนและลำดับของสีที่เหลือจากขวาไปซ้าย
+
+
+# class Stack:
+
+#     def __init__(self):
+
+#     def push(self, value):
+
+#     def pop(self):
+
+#     def size(self):
+
+#     def isEmpty(self):
+
+
+
+# inp = input('Enter Input : ').split()
+
+# S = Stack()
+
+# ### Enter Your Code Here ###
+
+# print(S.size())
+
+# ### Enter Your Code Here ###
+
+
+# Testcase : #1 
+# Enter Input : G H H H H P
+# 3
+# PHG
+
+# Testcase : #2 
+# Enter Input : L C C X X X C D
+# 2
+# DL
+# Combo : 2 ! ! !
+
+# Testcase : #3 
+# Enter Input : C C C
+# 0
+# Empty
+
+# Testcase : #4 
+# Enter Input : A
+# 1
+# A
+
+# Testcase : #5 
+# Enter Input : A B B A
+# 4
+# ABBA
+
+# Testcase : #6 
+# Enter Input : O O P Y Y E R R R E E Y P P K K K O
+# 0
+# Empty
+# Combo : 6 ! ! !
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, value):
+        self.items.append(value)
+
+    def pop(self):
+        if not self.isEmpty():
+            return self.items.pop()
+        return None
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.items[-1]
+        return None
+
+    def size(self):
+        return len(self.items)
+
+    def isEmpty(self):
+        return len(self.items) == 0
+
+
+def color_crush(inp):
+    S = Stack()
+    combo = 0
+
+    for color in inp:
+        S.push(color)
+        
+        while True:
+            if S.size() < 3:
+                break
+            
+            top = []
+            for i in range(3):
+                if S.size() > i:
+                    top.append(S.items[-(i + 1)])
+                else:
+                    break
+
+            if len(top) == 3 and top[0] == top[1] == top[2]:
+                for _ in range(3):
+                    S.pop()
+                combo += 1
+            else:
+                break
+
+    remaining_colors = []
+    while not S.isEmpty():
+        remaining_colors.append(S.pop())
+
+    print(len(remaining_colors))
+    if len(remaining_colors) > 0:
+        print(''.join(remaining_colors))
+    else:
+        print('Empty')
+
+    if combo >= 2:
+        print(f'Combo : {combo} ! ! !')
+
+inp = input('Enter Input : ').split()
+color_crush(inp)
---
+++ review-BF-test/stack04.py
@@ -0,0 +1,149 @@
+# Chapter : 3 - item : 4 - Stack Calculator
+
+# ให้เขียน class calculator  ที่มีการทำงานผ่านฟังก์ชัน run(instructions) โดยมี instructions ดังนี้
+# +: Pop 2 ค่าออกจาก stack, นำมาบวกกัน และ push ผลลัพธ์ลง stack
+# -: Pop 2 ค่าออกจาก stack, นำค่าที่อยู่ข้างบนลบด้วยค่าที่อยู่ข้างล่าง และ push ผลลัพธ์ลง stack
+# *: Pop 2 ค่าออกจาก stack, นำมาคูณกัน และ push ผลลัพธ์ลง stack
+# /: Pop 2 ค่าออกจาก stack, นำค่าที่อยู่ข้างบนหารด้วยค่าที่อยู่ข้างล่าง และ push ผลลัพธ์ลง stack
+# DUP: Duplicate (not double) ค่าบนสุดของ stack
+# POP: Pop ค่าบนสุดออกจาก stack และ discard.
+# PSH: ทำการ push ตัวเลขลง stack
+# หมายเหตุ 1. คำสั่งอื่น ๆ (เช่นตัวอักษร) ควรทำให้เกิดค่า "Invalid instruction: [instruction]"
+# 2. การนำค่าออกจาก stack สำหรับ calculator นี้ให้ การนำค่าออกจาก stack ครั้งแรกเป็น operand ด้านซ้าย และ การนำค่าออกจาก stack ครั้งที่ 2 เป็น operand ด้านขวา
+# *************************************************
+# print("* Stack Calculator *")
+# arg = input("Enter arguments : ")
+# machine = StackCalc()
+# machine.run(arg)
+# print(machine.getValue())
+
+# Testcase : #1 1
+# * Stack Calculator *
+# Enter arguments : 5 6 +
+# 11
+
+# Testcase : #2 2
+# * Stack Calculator *
+# Enter arguments : 3 DUP +
+# 6
+
+# Testcase : #3 3
+# * Stack Calculator *
+# Enter arguments : 6 5 5 7 * - /
+# 5
+
+# Testcase : #4 4
+# * Stack Calculator *
+# Enter arguments : a b c +
+# Invalid instruction: a
+
+# Testcase : #5 5
+# * Stack Calculator *
+# Enter arguments : 12
+# 12
+
+# Testcase : #6 6
+# * Stack Calculator *
+# Enter arguments : 9 14 DUP + - 3 POP
+# 19
+
+# Testcase : #7 7
+# * Stack Calculator *
+# Enter arguments : 1 2 3 4 5 POP POP POP
+# 2
+
+# Testcase : #8 8
+# * Stack Calculator *
+# Enter arguments : 13 DUP 4 POP 5 DUP + DUP + -
+# 7
+
+# Testcase : #9 9
+# * Stack Calculator *
+# Enter arguments : 4 POP
+# 0
+
+class StackCalc:
+    def __init__(self):
+        self.stack = []
+        self.error = None
+
+    def run(self, instructions):
+        for instruction in instructions.split():
+            if instruction.isdigit() or (instruction[0] == '-' and instruction[1:].isdigit()):  # Handling negative numbers
+                self.stack.append(int(instruction))
+            elif instruction == '+':
+                self._add()
+            elif instruction == '-':
+                self._subtract()
+            elif instruction == '*':
+                self._multiply()
+            elif instruction == '/':
+                self._divide()
+            elif instruction == 'DUP':
+                self._duplicate()
+            elif instruction == 'POP':
+                self._pop()
+            else:
+                self.error = f"Invalid instruction: {instruction}"
+                break
+
+    def _add(self):
+        if len(self.stack) >= 2:
+            a = self.stack.pop()
+            b = self.stack.pop()
+            self.stack.append(a + b)
+        else:
+            self.error = "Invalid instruction: +"
+
+    def _subtract(self):
+        if len(self.stack) >= 2:
+            a = self.stack.pop()
+            b = self.stack.pop()
+            self.stack.append(a - b)
+        else:
+            self.error = "Invalid instruction: -"
+
+    def _multiply(self):
+        if len(self.stack) >= 2:
+            a = self.stack.pop()
+            b = self.stack.pop()
+            self.stack.append(a * b)
+        else:
+            self.error = "Invalid instruction: *"
+
+    def _divide(self):
+        if len(self.stack) >= 2:
+            a = self.stack.pop()
+            b = self.stack.pop()
+            if b == 0:
+                self.error = "Invalid instruction: division by zero"
+            else:
+                self.stack.append(int(a / b))
+        else:
+            self.error = "Invalid instruction: /"
+
+    def _duplicate(self):
+        if len(self.stack) >= 1:
+            self.stack.append(self.stack[-1])
+        else:
+            self.error = "Invalid instruction: DUP"
+
+    def _pop(self):
+        if len(self.stack) >= 1:
+            self.stack.pop()
+        else:
+            self.error = "Invalid instruction: POP"
+
+    def getValue(self):
+        if self.error:
+            return self.error
+        if self.stack:
+            return self.stack[-1]
+        else:
+            return 0
+
+print("* Stack Calculator *")
+arg = input("Enter arguments : ")
+machine = StackCalc()
+machine.run(arg)
+print(machine.getValue())
